f408f2a317e5d683d8952a8d1e18550b2cfb73f4
Henrik Schubert <henrik.27@hotmail.de>
2024-10-21 14:40:23 +0100

    Translate to object-oriented programming

--- code/training_analysis.py
+++ code/training_analysis.py
@@ -6,19 +6,6 @@ import os
 ## Functions -------------------
 
 
-# Translate mile to kilometer
-def mile_km(mile):
-    return mile / 0.62
-
-# Clean the pace
-def clean_pace(pace):
-    pace = pace.str.replace(":", ".")
-    return pd.to_numeric(pace)
-
-# Translate minutes/mile to minutes/kilometer
-def mile_km_pace(pace):
-    return pace * 0.62
-
 # Basic plotting function
 def plot_running(y, ylab):
     fig, ax = plt.subplots()
@@ -28,19 +15,78 @@ def plot_running(y, ylab):
             xmax = mile_km(total.Distance),
             color = "black",
             alpha = 0.5)
-    ax.xlabel("Distance (km)")
-    ax.ylabel(ylab)
-    ax.show()
-    return ax
+    plt.xlabel("Distance (km)")
+    plt.ylabel(ylab)
+    plt.show()
+    return plt
+
+# Clean the pace
+def clean_pace(pace):
+    pace = pace.str.replace(":", ".")
+    return pd.to_numeric(pace)
+
+# Translate mile to kilometer
+def mile_km(mile):
+    return mile / 0.62
+
+    
+# Translate minutes/mile to minutes/kilometer
+def mile_km_pace(pace):
+    return pace * 0.62
+
 
 # Function to summarize the session
 # Create a class object that summarizes the training session
 # Create the most important plots
-#class training_report():
+class training_report:
+    def __init__(self, file_path):
+        """Initialize the data"""
+        self.df = pd.read_csv(file_path)
+  
+    # Create the kilometer column
+    def clean_data(self):
+        self.df["kilometer"] = self.df["Distance"].apply(mile_km)
+        self.df["pace"] = clean_pace(self.df["Avg Pace"])
+        self.total = self.df[self.df["Laps"] == "Summary"]
+        self.df = self.df[self.df["Laps"] != "Summary"]
+        
+    # Return the summary report
+    def summary(self):
+        self.report = {}
+        self.report["Distance"] = self.total["kilometer"]
+        self.report["pace"] = self.df["pace"].describe()
+        self.report["heart rate"] = self.df["Avg HR"].describe()
+        self.report["cadence"] = self.df["Max Run Cadence"].describe()
+        print(self.report)
     
+    # Plot the data
+    def plot_running(self, y, ylab):
+        """Plot of a running variable running"""
+        fig, ax = plt.subplots()
+        ax.step(self.df.kilometer.cumsum(), y)
+        ax.hlines(y = y.mean(), 
+                xmin = df.kilometer.min(),
+                xmax = mile_km(total.Distance),
+                color = "black",
+                alpha = 0.5)
+        plt.xlabel("Distance (km)")
+        plt.ylabel(ylab)
+        plt.show()
+        return plt
+        
 
-    
-    
+# Load the data
+training = training_report("../data/activity_17330403407.csv")
+training.clean_data()
+training.summary()
+training.plot_running(training.df["Avg HR"], "heart rate")
+
+
+# Apply the class
+today = training_report()
+
+today = training_report(df)
+today = training_report.from_csv("../data/activity_17330403407.csv")
 
 
 ## Data preparation ------------
@@ -49,18 +95,12 @@ def plot_running(y, ylab):
 file = os.listdir("data")
 df = pd.read_csv("data/" + file[0])
 
-# Create the kilometer column
-df.kilometer = df["Distance"].apply(mile_km)
-
-# Clean the pace vector
-df["pace"] = clean_pace(df["Avg Pace"])
-
 # Split the kilometers from total
 total = df[df["Laps"] == "Summary"]
 df = df[df["Laps"] != "Summary"]
 
 ## Summary statistics -----------
-
+#
 # Summarise the distance
 mile_km(total.Distance)
 
